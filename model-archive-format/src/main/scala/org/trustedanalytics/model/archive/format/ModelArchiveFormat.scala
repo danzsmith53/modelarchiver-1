3dd0b81d6a4c7c6ed12f57c232286a48ad6a2aa9
Anjali Sood <anjali.sood@intel.com>
2016-07-18 12:24:54 -0700

    Adding implementation of Model Archive Format to repo

---
+++ model-archive-format/src/main/scala/org/trustedanalytics/model/archive/format/ModelArchiveFormat.scala
@@ -0,0 +1,274 @@
+/**
+ *  Copyright (c) 2015 Intel Corporation 
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.trustedanalytics.model.archive.format
+
+import java.io._
+import java.net.{ URL, URLClassLoader }
+import java.nio.file.{ Files, Path }
+import java.util.zip.{ ZipInputStream, ZipEntry, ZipOutputStream }
+
+import com.typesafe.config.ConfigFactory
+import org.apache.commons.io.{ FileUtils, IOUtils }
+import org.trustedanalytics.atk.event.EventLogging
+import org.trustedanalytics.scoring.interfaces.{Model, ModelLoader}
+
+/**
+ * Read/write for publishing models
+ */
+
+object ModelArchiveFormat extends EventLogging {
+
+  val modelDataString = "modelData"
+  val modelReaderString = "modelReader"
+  val BUFFER_SIZE = 4096
+
+  /**
+   * Write a Model to a our special format that can be read later by a Scoring Engine.
+   *   
+   * @param classLoaderFiles list of jars and other files for ClassLoader
+   * @param modelLoaderClass class that implements the ModelLoader trait for instantiating the model during read()
+   * @param modelData the trained model data
+   * @param outputStream location to store published model
+   */
+  def write(classLoaderFiles: List[File], modelLoaderClass: String, modelData: Array[Byte], outputStream: FileOutputStream): Unit = {
+    val zipFile = new ZipOutputStream(new BufferedOutputStream(outputStream))
+
+    try {
+      classLoaderFiles.foreach((file: File) => {
+        if (!file.isDirectory && file.exists()) {
+          addFileToZip(zipFile, file)
+        }
+      })
+
+      addByteArrayToZip(zipFile, modelDataString + ".txt", modelData.length, modelData)
+      addByteArrayToZip(zipFile, modelReaderString + ".txt", modelLoaderClass.length, modelLoaderClass.getBytes("utf-8"))
+    }
+    catch {
+      case e: Exception =>
+        error("writing model failed", exception = e)
+        throw e
+    }
+    finally {
+      zipFile.finish()
+      IOUtils.closeQuietly(zipFile)
+    }
+  }
+
+  /**
+   * Read a Model from our special format using a private ClassLoader.
+   *   
+   * May throw exception if version of archive doesn't match current library.
+   *
+   * @param modelArchiveInput location to read published model from
+   * @param parentClassLoader parentClassLoader to use for the private ClassLoader
+   * @return the instantiated Model   
+   */
+  def read(modelArchiveInput: File, parentClassLoader: ClassLoader): Model = {
+    var zipInputStream: ZipInputStream = null
+    var modelName: String = null
+    var urls = Array.empty[URL]
+    var byteArray: Array[Byte] = null
+    var libraryPaths: Set[String] = Set.empty[String]
+
+    try {
+      // Extract files to temporary directory so that dynamic library names are not changed
+      val tempDirectory = getTemporaryDirectory
+      zipInputStream = new ZipInputStream(new FileInputStream(modelArchiveInput))
+
+      var entry = zipInputStream.getNextEntry
+      while (entry != null) {
+        val individualFile = entry.getName
+        val file = extractFile(zipInputStream, tempDirectory.toString, individualFile)
+
+        if (individualFile.contains(".jar")) {
+          val url = file.toURI.toURL
+          urls = urls :+ url
+        }
+        else if (individualFile.contains(".so")) {
+          libraryPaths += getDirectoryPath(file)
+        }
+        else if (individualFile.contains(".dll")) {
+          libraryPaths += getDirectoryPath(file)
+        }
+        else if (individualFile.contains(modelReaderString)) {
+          val s = scala.io.Source.fromFile(tempDirectory.toString + "/" + individualFile).mkString
+          modelName = s.replaceAll("\n", "")
+        }
+        else if (individualFile.contains(modelDataString)) {
+          byteArray = scala.io.Source.fromFile(tempDirectory.toString + "/" + individualFile).map(_.toByte).toArray
+        }
+        entry = zipInputStream.getNextEntry
+      }
+
+      val classLoader = new URLClassLoader(urls, parentClassLoader)
+      val modelLoader = classLoader.loadClass(modelName).newInstance()
+
+      addToJavaLibraryPath(libraryPaths) //Add temporary directory to java.library.path
+      modelLoader.asInstanceOf[ModelLoader].load(byteArray)
+    }
+    catch {
+      case e: Exception =>
+        error("reading model failed", exception = e)
+        throw e
+    }
+    finally {
+      IOUtils.closeQuietly(zipInputStream)
+    }
+  }
+
+  /**
+   * Write content to temporary file
+   *
+   * @param zipIn File content to write
+   * @param tempDir Temporary directory
+   * @param filePath File path
+   *
+   * @return Temporary file
+   */
+  def extractFile(zipIn: ZipInputStream, tempDir: String, filePath: String): File = {
+    var file: File = null
+    val fileName = filePath.substring(filePath.lastIndexOf("/") + 1)
+    var bufferedOutStream: BufferedOutputStream = null
+
+    try {
+      file = new File(tempDir, fileName)
+      file.createNewFile()
+
+      bufferedOutStream = new BufferedOutputStream(new FileOutputStream(file))
+      val bytesIn: Array[Byte] = new Array[Byte](BUFFER_SIZE)
+      var continueReading = true
+      var read = 0
+      while (continueReading) {
+        read = zipIn.read(bytesIn)
+        if (read != -1) {
+          bufferedOutStream.write(bytesIn, 0, read)
+        }
+        else {
+          continueReading = false
+        }
+      }
+    }
+    catch {
+      case e: Exception =>
+        error(s"reading model failed due to error extracting file: ${filePath}", exception = e)
+        throw e
+    }
+    finally {
+      bufferedOutStream.close()
+    }
+    file
+  }
+
+  /**
+   * Add byte array contents to zip file using
+   *
+   * @param zipFile Zip File
+   * @param entryName Name of entry to add
+   * @param entrySize Size of entry
+   * @param entryContent Content to add
+   */
+  def addByteArrayToZip(zipFile: ZipOutputStream, entryName: String, entrySize: Int, entryContent: Array[Byte]): Unit = {
+    val modelEntry = new ZipEntry(entryName)
+    modelEntry.setSize(entrySize)
+    zipFile.putNextEntry(modelEntry)
+    IOUtils.copy(new ByteArrayInputStream(entryContent), zipFile)
+  }
+
+  /**
+   * Add file contents to the zip file
+   *
+   * @param zipFile Zip file
+   * @param file File to add
+   */
+  def addFileToZip(zipFile: ZipOutputStream, file: File): Unit = {
+    val fileEntry = new ZipEntry(file.getName)
+    try {
+      zipFile.putNextEntry(fileEntry)
+      IOUtils.copy(new FileInputStream(file), zipFile)
+    }
+    catch {
+      case e: Exception => {
+        error("Failed to add the given file to zip ", exception = e)
+      }
+    }
+  }
+
+  def getTemporaryDirectory: Path = {
+    try {
+      val config = ConfigFactory.load(this.getClass.getClassLoader)
+      val configKey = "atk.scoring-engine.tmpdir"
+
+      val tmpModelDir = if (config.hasPath(configKey)) {
+        val tmpDirStr: String = config.getString("atk.scoring-engine.tmpdir")
+        val tmpDir = new File(tmpDirStr)
+        if (!tmpDir.exists()) {
+          tmpDir.mkdir()
+        }
+        tmpDir
+      }
+      else {
+        val tmpDir = Files.createTempDirectory("tap-scoring-model")
+        tmpDir.toFile
+      }
+
+      info(s"installing model to temporary directory:${tmpModelDir.getAbsolutePath}")
+      sys.addShutdownHook(FileUtils.deleteQuietly(tmpModelDir)) // Delete temporary directory on exit
+      tmpModelDir.toPath
+    }
+    catch {
+      case e: Exception => {
+        error("Failed to create temporary director for extracting model", exception = e)
+        throw e
+      }
+    }
+  }
+
+  /**
+   * Get directory path for input file
+   *
+   * @param file Input file
+   * @return Returns parent directory if input is a file, or absolute path if input is a directory
+   */
+  private def getDirectoryPath(file: File): String = {
+    if (file.isDirectory) {
+      file.getAbsolutePath
+    }
+    else {
+      file.getParent
+    }
+  }
+
+  /**
+   * Dynamically add library paths to java.library.path
+   *
+   * @param libraryPaths Library paths to add
+   */
+  private def addToJavaLibraryPath(libraryPaths: Set[String]): Unit = {
+    try {
+      val usrPathField = classOf[ClassLoader].getDeclaredField("usr_paths")
+      usrPathField.setAccessible(true)
+      val newLibraryPaths = usrPathField.get(null).asInstanceOf[Array[String]].toSet ++ libraryPaths
+      usrPathField.set(null, newLibraryPaths.toArray)
+    }
+    catch {
+      case e: Exception =>
+        error(s"reading model failed due to failure to set java.library.path: ${libraryPaths.mkString(",")}",
+          exception = e)
+        throw e
+    }
+  }
+
+}
---
+++ model-archive-format/src/test/scala/org/trustedanalytics/model/archive/format/ModelArchiveFormatTest.scala
@@ -0,0 +1,145 @@
+/**
+ *  Copyright (c) 2015 Intel Corporation 
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.trustedanalytics.model.archive.format
+
+import java.io._
+import java.nio.charset.Charset
+import java.util.zip.{ ZipOutputStream, ZipInputStream }
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry
+import org.scalatest.WordSpec
+import org.apache.commons.io.{ FileUtils, IOUtils }
+import org.scalatest.Assertions._
+import org.trustedanalytics.scoring.interfaces.{Field, ModelMetaDataArgs, Model, ModelLoader}
+
+class ModelArchiveFormatTest extends WordSpec {
+
+  "ModelArchiveFormat" should {
+    "create a zip of given files and place into an output stream" in {
+      val testZip = File.createTempFile("test", ".jar")
+      val testZip2 = File.createTempFile("test2", ".jar")
+      val fileList = testZip :: testZip2 :: Nil
+      var zipFile: File = null
+      var zipOutput: FileOutputStream = null
+      var counter = 0
+      val modelReader = "TestModelReaderPlugin"
+      val model = "This is a test Model"
+
+      var testZipFileStream: ZipInputStream = null
+      try {
+        zipFile = File.createTempFile("TestZip", ".mar")
+        zipOutput = new FileOutputStream(zipFile)
+        ModelArchiveFormat.write(fileList, modelReader, model.getBytes(Charset.forName("utf-8")), zipOutput)
+        val tempDirectory = ModelArchiveFormat.getTemporaryDirectory
+        testZipFileStream = new ZipInputStream(new FileInputStream(new File(zipFile.getAbsolutePath)))
+
+        var entry = testZipFileStream.getNextEntry
+
+        while (entry != null) {
+          val individualFile = entry.getName
+          if (individualFile.contains(".jar")) {
+            counter = counter + 1
+          }
+          else if (individualFile.contains("modelReader")) {
+            val file = ModelArchiveFormat.extractFile(testZipFileStream, tempDirectory.toString, individualFile)
+            val s = scala.io.Source.fromFile(tempDirectory.toString + "/" + individualFile).mkString
+            val modelName = s.replaceAll("\n", "")
+            assert(modelName.equals(modelReader))
+          }
+          else if (individualFile.contains("modelData")) {
+            val file = ModelArchiveFormat.extractFile(testZipFileStream, tempDirectory.toString, individualFile)
+            val byteArray = scala.io.Source.fromFile(tempDirectory.toString + "/" + individualFile).map(_.toByte).toArray
+            assert(byteArray.length == model.getBytes(Charset.forName("utf-8")).length)
+          }
+          entry = testZipFileStream.getNextEntry
+        }
+        assert(counter == 2)
+      }
+      finally {
+        FileUtils.deleteQuietly(zipFile)
+        FileUtils.deleteQuietly(testZip)
+      }
+    }
+  }
+
+  "create a model given a mar file" in {
+    val testZipFile = File.createTempFile("TestZip", ".mar")
+    val testJarFile = File.createTempFile("test", ".jar")
+    val testZipArchive = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(testZipFile)))
+    var modelDataFile = File.createTempFile("modelData", ".txt")
+    var modelLoaderFile = File.createTempFile("modelReader", ".txt")
+
+    try {
+      ModelArchiveFormat.addFileToZip(testZipArchive, testJarFile)
+      ModelArchiveFormat.addByteArrayToZip(testZipArchive, "modelData.txt", 128, "This is dummy model data".getBytes("utf-8"))
+      ModelArchiveFormat.addByteArrayToZip(testZipArchive, "modelReader.txt", 256, "org.trustedanalytics.model.archive.format.TestModelReaderPlugin".getBytes("utf-8"))
+
+      testZipArchive.finish()
+      IOUtils.closeQuietly(testZipArchive)
+
+      val testModel = ModelArchiveFormat.read(testZipFile, this.getClass.getClassLoader)
+
+      assert(testModel.isInstanceOf[Model])
+      assert(testModel != null)
+    }
+    catch {
+      case e: Exception =>
+        throw e
+    }
+    finally {
+      FileUtils.deleteQuietly(modelLoaderFile)
+      FileUtils.deleteQuietly(modelDataFile)
+      FileUtils.deleteQuietly(testZipFile)
+      FileUtils.deleteQuietly(testJarFile)
+    }
+  }
+}
+
+class TestModelReaderPlugin extends ModelLoader {
+
+  private var testModel: TestModel = _
+
+  override def load(bytes: Array[Byte]): Model = {
+    testModel = new TestModel
+    testModel.asInstanceOf[Model]
+  }
+}
+
+class TestModel() extends Model {
+
+  override def score(data: Array[Any]): Array[Any] = {
+    var score = Array[Any]()
+    score = score :+ 2
+    score
+  }
+
+  override def input: Array[Field] = {
+    var input = Array[Field]()
+    input = input :+ Field("input", "Float")
+    input
+  }
+
+  override def output: Array[Field] = {
+    var output = Array[Field]()
+    output = output :+ Field("output", "Float")
+    output
+  }
+
+  override def modelMetadata(): ModelMetaDataArgs = {
+    new ModelMetaDataArgs("Dummy Model", "dummy class", "dummy reader", Map("created_on" -> "Jan 29th 2016"))
+  }
+}
+
